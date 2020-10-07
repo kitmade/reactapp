893c4846f1a5eb51da9e5eab2458b59b49349eba
kit nguyen <kit@Huys-MacBook-Pro.local>
2020-10-07 10:06:52 +0700

    install react-native-

--- ios/reactapp/AppDelegate.m
+++ ios/reactapp/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -11,6 +12,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -27,6 +29,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBXJON7dwHZMqwuA1aIj1VZlLEXyn0FuZY"];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View, SafeAreaView, Button} from 'react-native';
+import MapView from 'react-native-maps';
 import {Provider, useDispatch} from 'react-redux';
 import storeConfig from './redux/storeConfig';
 
@@ -9,17 +10,18 @@ const App = ({params}) => {
   //   const dispatch = useDispatch();
   return (
     <Provider store={store}>
-      <View>
-        <SafeAreaView>
-          <Text>App</Text>
-          <Button
-            title="Init"
-            //   onPress={() => {
-            //     dispatch();
-            //   }}
-          />
-        </SafeAreaView>
-      </View>
+      {/* <View> */}
+      <SafeAreaView style={{flex: 1}}>
+        <MapView style={{flex: 1}} />
+        <Text>App</Text>
+        <Button
+          title="Init"
+          //   onPress={() => {
+          //     dispatch();
+          //   }}
+        />
+      </SafeAreaView>
+      {/* </View> */}
     </Provider>
   );
 };
