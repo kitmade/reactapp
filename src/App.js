ddc0c16075b273b28268aba11bb8baae8bf45880
kit nguyen <kit@Huys-MacBook-Pro.local>
2020-10-07 10:54:58 +0700

    install vector-icons

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {Provider, useDispatch} from 'react-redux';
 import storeConfig from './redux/storeConfig';
 import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const store = storeConfig();
 
 const App = ({params}) => {
   //   const dispatch = useDispatch();
-  BackgroundGeolocation.start();
-  BackgroundGeolocation.checkStatus((status) => {
-    console.log(123, status);
-  });
+  // BackgroundGeolocation.start();
+  // BackgroundGeolocation.checkStatus((status) => {
+  //   console.log(123, status);
+  // });
   return (
     <Provider store={store}>
       {/* <View> */}
       <SafeAreaView style={{flex: 1}}>
-        <MapView style={{flex: 1}} provider={PROVIDER_GOOGLE} />
+        {/* <MapView style={{flex: 1}} provider={PROVIDER_GOOGLE} /> */}
         <Text>App</Text>
         <Button
           title="Init"
