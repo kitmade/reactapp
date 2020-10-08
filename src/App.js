0dd74af6152aeea5ee8616ee4ac842a39774bfcc
kit nguyen <kit@Huys-MacBook-Pro.local>
2020-10-08 11:24:07 +0700

    init redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import {Text, View, SafeAreaView, Button} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {Provider, useDispatch} from 'react-redux';
 import storeConfig from './redux/storeConfig';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import MainScreen from './screens/MainScreen';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import HomeScreen from './screens/HomeScreen';
+// import MainScreen from './screens/MainScreen';
 
 const store = storeConfig();
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 const App = ({params}) => {
-  //   const dispatch = useDispatch();
-  // BackgroundGeolocation.start();
-  // BackgroundGeolocation.checkStatus((status) => {
-  //   console.log(123, status);
-  // });
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <>
+        <View style={{flex: 1}}>
+          <Text>Hello</Text>
+        </View>
+      </>
+      {/* <NavigationContainer>
         <Stack.Navigator initialRouteName="Name">
           <Stack.Screen
             name="Home"
@@ -33,7 +32,7 @@ const App = ({params}) => {
             options={{headerShown: false}}
           />
         </Stack.Navigator>
-      </NavigationContainer>
+      </NavigationContainer> */}
     </Provider>
   );
 };
