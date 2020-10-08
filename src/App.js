75e7a3ace75a15d1933a972c118dbcb9ee89a8c9
kit nguyen <kit@Huys-MacBook-Pro.local>
2020-10-08 12:07:45 +0700

    install and config @mauron85/react-native-background-

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Provider, useDispatch} from 'react-redux';
 import storeConfig from './redux/storeConfig';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 // import HomeScreen from './screens/HomeScreen';
 // import MainScreen from './screens/MainScreen';
 
@@ -12,6 +13,9 @@ const store = storeConfig();
 const Stack = createStackNavigator();
 
 const App = ({params}) => {
+  BackgroundGeolocation.checkStatus((status) => {
+    console.log(123, status);
+  });
   return (
     <Provider store={store}>
       <NavigationContainer>
