99bd1dc5957b287996163f6145294759090289c2
kit nguyen <kit@Huys-MacBook-Pro.local>
2020-10-08 11:38:25 +0700

    install react-

--- src/App.js
+++ src/App.js
@@ -2,38 +2,32 @@ import React from 'react';
 import {Text, View, SafeAreaView, Button} from 'react-native';
 import {Provider, useDispatch} from 'react-redux';
 import storeConfig from './redux/storeConfig';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 // import HomeScreen from './screens/HomeScreen';
 // import MainScreen from './screens/MainScreen';
 
 const store = storeConfig();
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 const App = ({params}) => {
   return (
-    <Provider store={store}>
-      <>
-        <View style={{flex: 1}}>
-          <Text>Hello</Text>
-        </View>
-      </>
-      {/* <NavigationContainer>
-        <Stack.Navigator initialRouteName="Name">
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Main"
-            component={MainScreen}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer> */}
-    </Provider>
+    // <Provider store={store}>
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={() => (
+            <View style={{flex: 1}}>
+              <Text>Hello</Text>
+            </View>
+          )}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </Provider>
   );
 };
 
